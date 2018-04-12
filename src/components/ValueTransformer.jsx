f58c54f0848525da7cad3c10c82a00bc4449f4b5
Joshua Johnsgaard <joshua.johnsgaard@vizworx.com>
2018-04-12 12:05:33 -0600

    Adds value transformer component

---
+++ src/components/ValueTransformer.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import immutableObject from 'object-path-immutable';
+import ValueSubscriber from './ValueSubscriber';
+
+export default class ValueTransformer extends ValueSubscriber {
+  static propTypes = {
+    ...ValueSubscriber.propTypes,
+    transformOnChange: PropTypes.func.isRequired,
+    transformValue: PropTypes.func.isRequired,
+  }
+
+  static childContextTypes = {
+    ...ValueSubscriber.childContextTypes,
+    nfOnChange: PropTypes.func.isRequired,
+  }
+
+  getChildContext() {
+    return {
+      ...super.getChildContext(),
+      nfOnChange: e => this.onChange(e),
+    };
+  }
+
+  onChange(e) {
+    const name = e.target.name.slice(this.getName().length);
+    const value = immutableObject.set(this.getValue(), name, e.target.value);
+    const newValue = this.props.transformOnChange(value);
+    super.onChange(newValue);
+  }
+
+  getValue() {
+    const value = super.getValue();
+    return this.props.transformValue(value);
+  }
+}
